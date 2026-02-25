c53c3dab2139767f3a6eabcf71bac01e10ac91da
Filip <cvejn.fi.2024@skola.ssps.cz>
2026-02-25 09:35:36 +0100

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/ubytovna-u-vapenky/',
   server: {
     host: "::",
     port: 3000,
